7736ea8d2fdbae43da5d227de867b785cbf00652
alpenwasser <webmaster@alpenwasser.net>
2016-12-20 23:25:48 +0100

    Code Snippets for Report
    They still need to be cleaned up a bit though.
    
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   Main.ctr
    	modified:   Main.tex
    	modified:   Makefile
    	new file:   _minted-Main/01906C7D6AF7806D01CB3F76BE18421958C11C922EF2A6AFBD53E0A53EB66EB9.pygtex
    	new file:   _minted-Main/1085B80B6385FD7A27801A64127B7C494EEE172A3F9296B31A282D9AFEF27B06.pygtex
    	new file:   _minted-Main/1CBD20EE9E9F35B1463C25EF5D4215652F2964D83AF849E537C933D66EDC3CF1.pygtex
    	new file:   _minted-Main/5761E8BEE5826E41077C52FD1B81E32E871CB04B1E40D41F0899890BF4F75EAB.pygtex
    	new file:   _minted-Main/8C953EECABADC473BDC39808F419FCA0B5F3999738102AF91A6D06CEC90E316B.pygtex
    	new file:   _minted-Main/BE574F1F64CE68A50DE8DA20674951540E41CDFFA9B7F2DEFACEA13053E6018C.pygtex
    	new file:   _minted-Main/F0AAE47E3F567CF3303CE4A4CE2807E5034D4627C960BBF9502955E5A3DF0F32.pygtex
    	new file:   _minted-Main/borland.pygstyle
    	new file:   _minted-Main/default-pyg-prefix.pygstyle
    	new file:   appendices/scripts.tex
    	new file:   code/33120A.py
    	new file:   code/acquireWaveRunnerData.py
    	new file:   code/configWaveRunner.py
    	new file:   code/measure--gain01--1-1-3-11.sh
    	new file:   code/measure--sigdel--1-3-11.sh
    	modified:   mainmatter/testBench.tex
    	modified:   preamble/macros.tex
    	modified:   preamble/packages.tex
    	new file:   ../resources/device-manuals/MX100T+MX100TP_Instruction_Manual-Iss5.txt

---
+++ report/code/measure--sigdel--1-3-11.sh
@@ -0,0 +1,178 @@
+#!/usr/bin/env bash
+
+# ---------------------------------------------------------------------------- #
+# DESCRIPTION                                                                  #
+# ---------------------------------------------------------------------------- #
+# This script executes a set of measurements for
+# 1 chip
+# at three sampling frequencies
+# for 11 DC voltages on the input
+# (hence the name)
+
+# ---------------------------------------------------------------------------- #
+# USAGE                                                                        #
+# ---------------------------------------------------------------------------- #
+# Configure the settings down below and execute:
+# ./measure--1-3-11.sh
+# The oscilloscope will require some manual intervention at the beginning, for
+# which this script will prompt you  before continuing. So don't just start it
+# and run off to get coffee.
+
+# ---------------------------------------------------------------------------- #
+# DEPENDENCIES                                                                 #
+# ---------------------------------------------------------------------------- #
+# The following  python scripts must  be placed in  the same
+# directory as this script:
+#
+# 33120A.py                # control the function generators for VIN and CLK
+
+# ---------------------------------------------------------------------------- #
+# SETTINGS                                                                     #
+# ---------------------------------------------------------------------------- #
+# Adjust these as needed...
+CHIP='chip01'
+DATA_DIR="data/${CHIP}"
+
+# ---------------------------------------------------------------------------- #
+# SETUP AND NOTIFICATIONS                                                      #
+# ---------------------------------------------------------------------------- #
+
+IFS='' read -r -d '' chip_msg <<-EOF
+	Chip and Gain Selection
+	========================================================
+	Measuring ${CHIP}
+EOF
+IFS='' read -r -d '' good_to_go <<-'EOF'
+	Commencing Automated Measuring
+	========================================================
+	No further manual intervention is required from here on.
+	We recommend grabbing a cup of coffee or something like 
+	that.
+	                          {
+	                       }   }   {
+	                      {   {  }  }
+	                       }   }{  {
+	                      {  }{  }  }
+	                     ( }{ }{  { )
+	                    .-{   }   }-.
+	                    ( ( } { } { } )
+	                    |`-.._____..-'|
+	                    |             ;--.
+	                    |   (__)     (__  \
+	                    |   (oo)      | )  )
+	                    |    \/       |/  /
+	                    |             /  /
+	                    |            (  /
+	                    \             y'
+	                    `-.._____..-'
+	
+EOF
+#(Design by Felix Lee, http://www.ascii-art.de/ascii/c/coffee.txt)
+
+# Initialize the osccilloscope
+
+printf '%s\n%s' "$chip_msg" "$good_to_go"
+
+# ---------------------------------------------------------------------------- #
+# CONFIGURATION DATA                                                           #
+# ---------------------------------------------------------------------------- #
+# These settings are mainly used for the configWaveRunner.py script
+
+# Time Bases:
+#  32 kHz: 5us
+#  96 kHz: 2.5us
+# 256 kHz: 1us
+declare -A timeDivs
+timeDivs[32e3]='5'
+timeDivs[96e3]='2.5'
+timeDivs[256e3]='1'
+
+declare -A fsStrings
+fsStrings[32e3]='032kHz'
+fsStrings[96e3]='096kHz'
+fsStrings[256e3]='256kHz'
+
+# ----------------------------------
+# Display settings for osccilloscope
+# ----------------------------------
+# NOTE: The  boundaries and  scaling of  the  downloaded data  depend  on  the
+# oscilloscope's  display settings,  which is  why we  need these  despite not
+# actually caring much about the oscilloscope's display.
+# For GAIN +1
+# DC Voltage | MVOLT_DIV | OFFSET MV |
+#        0.5 |       200 |     -1000 |
+#        0.7 |       200 |     -1100 |
+#        0.9 |       100 |     -1200 |
+#        1.1 |       100 |     -1300 |
+#        1.3 |        50 |     -1400 |
+#        1.5 |        20 |     -1500 |
+#        1.7 |        50 |     -1600 |
+#        1.9 |       100 |     -1700 |
+#        2.1 |       100 |     -1800 |
+#        2.3 |       200 |     -1900 |
+#        2.5 |       200 |     -2000 |
+# Reverse order of table for GAIN -1
+
+declare -a ampls=(\
+    0.5\
+    0.7\
+    0.9\
+    1.1\
+    1.3\
+    1.5\
+    1.7\
+    1.9\
+    2.1\
+    2.3\
+    2.5\
+)
+
+declare -A voltDivs
+voltDivs[0.5]='200'
+voltDivs[0.7]='200'
+voltDivs[0.9]='100'
+voltDivs[1.1]='100'
+voltDivs[1.3]='50'
+voltDivs[1.5]='20'
+voltDivs[1.7]='50'
+voltDivs[1.9]='100'
+voltDivs[2.1]='100'
+voltDivs[2.3]='200'
+voltDivs[2.5]='200'
+
+declare -A offsets
+offsets[+0.5]='-1000'
+offsets[+0.7]='-1100'
+offsets[+0.9]='-1200'
+offsets[+1.1]='-1300'
+offsets[+1.3]='-1400'
+offsets[+1.5]='-1500'
+offsets[+1.7]='-1600'
+offsets[+1.9]='-1700'
+offsets[+2.1]='-1800'
+offsets[+2.3]='-1900'
+offsets[+2.5]='-2000'
+
+# ---------------------------------------------------------------------------- #
+# DO THE STUFF                                                                 #
+# ---------------------------------------------------------------------------- #
+# Measure ALL the things!!!
+mkdir -p "$DATA_DIR"
+for clk in 32e3 96e3 256e3;do
+    ./33120A.py --clock=$clk
+    # Give the function generator time to settle:
+    sleep 0.25 
+
+    # Run through DC ramp
+    for ampl in "${ampls[@]}";do
+        ./33120A.py --voltage="$ampl"
+        sleep 0.25
+
+        printf 'Acquiring trace for %s Hz and %s V\n' $clk $ampl
+        local_file="${DATA_DIR}/${CHIP}-${fsStrings[$clk]}-${ampl}V.txt"
+        ssh -p 22 pi@10.84.130.16 'cd bitstream/build && sudo ./bitstream'
+        scp -P 22 \
+            pi@10.84.130.16:/home/pi/bitstream/build/bit_stream.txt \
+            "$local_file"
+    done
+done
