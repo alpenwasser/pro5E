782226a349dbc6e36c3d10bb5d3c5d1b920ca985
alpenwasser <webmaster@alpenwasser.net>
2016-12-21 10:38:30 +0100

    Digital Data: Processed, Plots in Appendix

--- report/data/linkData.sh
+++ report/data/linkData.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+# Preamp -------------------------------------------------------------------- #
 declare -a filelist
 while IFS= read -r -d '' element;do
 	filelist+=( "$element" )
-done < <( find "$(pwd)/../../measurements--"*"/data" -type f -print0 )
+done < <( find "$(pwd)/../../measurements--preamp/data" -type f -print0 )
 
 for file in "${filelist[@]}";do
 	targetName="${file#${PWD}/}"
@@ -17,3 +18,44 @@ for file in "${filelist[@]}";do
 	mkdir -p "$linkDir"
 	ln -s "$targetName" "$linkPath"
 done
+unset filelist
+
+# SigDel -------------------------------------------------------------------- #
+declare -a filelist
+while IFS= read -r -d '' element;do
+	filelist+=( "$element" )
+done < <( find "$(pwd)/../../measurements--sigdel/data-processed" -type f -print0 )
+
+for file in "${filelist[@]}";do
+	targetName="${file#${PWD}/}"
+	targetName="../../${targetName}"
+	linkName="${targetName##*/}"
+	linkDir="${targetName%/*}"
+	linkDir="${linkDir#../../../../measurements--}"
+	linkDir="${linkDir/\/data-processed\///}"
+	linkPath="${linkDir}/${linkName/txt/dat}"
+
+	mkdir -p "$linkDir"
+	ln -s "$targetName" "$linkPath"
+done
+unset filelist
+
+# Both ---------------------------------------------------------------------- #
+declare -a filelist
+while IFS= read -r -d '' element;do
+	filelist+=( "$element" )
+done < <( find "$(pwd)/../../measurements--both/data-processed" -type f -print0 )
+
+for file in "${filelist[@]}";do
+	targetName="${file#${PWD}/}"
+	targetName="../../${targetName}"
+	linkName="${targetName##*/}"
+	linkDir="${targetName%/*}"
+	linkDir="${linkDir#../../../../measurements--}"
+	linkDir="${linkDir/\/data-processed\///}"
+	linkPath="${linkDir}/${linkName/txt/dat}"
+
+	mkdir -p "$linkDir"
+	ln -s "$targetName" "$linkPath"
+done
+unset filelist
