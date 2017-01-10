ba5d0b759a491bfb39738e96670b156a58cfeb03
TheComet <alex.murray@gmx.ch>
2017-01-10 18:52:22 +0100

    Most of the DC measurements are parsed

--- measurements/evaluation/dc_evaluation.py
+++ measurements/evaluation/dc_evaluation.py
@@ -3,51 +3,70 @@ import deltasigma as ds
 import numpy as np
 import os
 import re
-import xml.etree.ElementTree as ET
 
 
-def evaluate():
-    root_node = ET.Element('chips')
-    for configuration in ['both']:  # ['both', 'both-manual', 'preamp', 'sigdel']:
+def evaluate(soup):
+    for configuration in ('both', 'both-manual', 'sigdel'):
         data_path = os.path.join('measurements-dc', configuration, 'data')
         for root, dirs, files in os.walk(data_path):
             for d in dirs:
                 match = re.match('chip([0-9]+)', d)
                 if match is None:
                     continue
+
                 chip_id = match.group(1).lstrip('0')
-                chip_node = ET.SubElement(root_node, 'chip')
-                chip_node.set('id', chip_id)
-                configuration_node = ET.SubElement(chip_node, 'configuration')
-                configuration_node.set('name', configuration)
-                measurement_node = ET.SubElement(configuration_node, 'measurement')
-                measurement_node.set('type', 'dc')
-                evaluate_chip(os.path.join(data_path, d), configuration, measurement_node)
+                chip_node = soup.chips.find('chip', id=chip_id)
+                if chip_node is None:
+                    chip_node = soup.new_tag('chip', id=chip_id)
+                    soup.chips.append(chip_node)
 
-    open('processed_measurements.xml', 'w').write(ET.dump(root_node))
+                configuration_node = chip_node.find('configuration', configuration=configuration, type='dc')
+                if configuration_node is None:
+                    configuration_node = soup.new_tag('configuration', configuration=configuration, type='dc')
+                    chip_node.append(configuration_node)
+
+                evaluate_chip(os.path.join(data_path, d), configuration, soup, configuration_node)
 
 
-def evaluate_chip(chip_dir_name, configuration, measurement_node):
+def evaluate_chip(chip_dir_name, configuration, soup, configuration_node):
     for root, dirs, files in os.walk(chip_dir_name):
         for file in files:
-            # apparently, we can't keep our file naming consistent
-            if configuration in ('both', 'both-manual'):
-                match_dc = re.match('.*?([0-9]\.[0-9]+)V.*', file)
-                match_fs = re.match('.*?([0-9]+)kHz.*', file)
-            elif configuration == 'preamp':
-                pass
+            match_dc = re.match('.*?([0-9]\.[0-9]+)V.*', file)
+            match_fs = re.match('.*?([0-9]+)kHz.*', file)
 
-            if match_dc is None or match_fs is None:
+            # gain sign was only tested in preamp configuration
+            if configuration == 'preamp':
+                match_sign = re.match('.*gain([\+-])[0-9]+.*', file)
+            else:
+                match_sign = re.match('(.*)', '+')  # default was positive
+
+            # gain is 1 if measuring only the sigdel
+            if configuration == 'sigdel':
+                match_gain = re.match('(.*)', '1')
+            else:
+                match_gain = re.match('.*gain[\+-]?([0-9]+).*', file)
+
+            if match_dc is None or match_fs is None or match_gain is None or match_sign is None:
+                print('WARNING: Skipping file {}'.format(file))
                 continue
-
-            print('evaluating DC for {}/{}'.format(chip_dir_name, file))
 
             current_dc = match_dc.group(1).lstrip('0')
             current_fs = match_fs.group(1).lstrip('0')
-            measurement_node.set('fs', current_fs)
-            measurement_node.set('gain', '1')
-            ET.SubElement(measurement_node, 'input').text = current_dc
-            ET.SubElement(measurement_node, 'output').text = str(bit_stream_to_dc(os.path.join(chip_dir_name, file)))
+            current_gain = match_gain.group(1).lstrip('0')
+            current_sign = match_sign.group(1)
+
+            print('evaluating DC for {}/{}, sign={}, gain={}, fs={}, dc={}'.format(chip_dir_name, file, current_sign, current_gain, current_fs, current_dc))
+
+            measurement_node = configuration_node.find('measurement', fs=current_fs, gain=current_gain, sign=current_sign)
+            if measurement_node is None:
+                measurement_node = soup.new_tag('measurement', fs=current_fs, gain=current_gain, sign=current_sign)
+                configuration_node.append(measurement_node)
+
+            measured_dc = str(bit_stream_to_dc(os.path.join(chip_dir_name, file)))
+            value_node = soup.new_tag('value', input=current_dc,
+                                               output=measured_dc)
+
+            measurement_node.append(value_node)
 
 
 def bit_stream_to_dc(file_name):
--- measurements/parse_all_measurements.py
+++ measurements/parse_all_measurements.py
@@ -1,5 +1,11 @@
+from bs4 import BeautifulSoup
 from evaluation import dc_evaluation
 
 
 if __name__ == '__main__':
-    dc_evaluation.evaluate()
+    soup = BeautifulSoup('<chips />', 'xml')
+    #soup = BeautifulSoup(open('processed_measurements.xml', 'r'), 'xml')
+
+    dc_evaluation.evaluate(soup)
+
+    open('processed_measurements.xml', 'wb').write(soup.prettify("utf-8"))
