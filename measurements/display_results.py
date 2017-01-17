90a931039308d13996b7f687828f546c9c5ab47a
TheComet <alex.murray@gmx.ch>
2017-01-17 02:42:07 +0100

    slope vs fs

--- measurements/display_results.py
+++ measurements/display_results.py
@@ -1,18 +1,27 @@
-import json
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from evaluation.fit_functions import linear_function
 from bs4 import BeautifulSoup
+
+
+def avg_std(values, uncertainties):
+    weights = 1 / np.power(uncertainties, 2)
+    avg = np.sum([value * weight for value, weight in zip(values, weights)]) / np.sum(weights)
+    uncertainty = 1 / np.sqrt(np.sum(weights))
+    return avg, uncertainty
 
 
 def get_linearity_results(soup, configuration, fs):
     for chip_node in soup.chips.find_all('chip'):
         for configuration_node in chip_node.find_all('configuration', configuration=configuration, type='dc'):
             for measurement_node in configuration_node.find_all('measurement', fs=str(fs), gain='1', sign='+'):
+                input_voltage = [float(x) for x in measurement_node.input_voltage['voltage'].split(',')]
                 offset = (float(measurement_node.fit['b0']), float(measurement_node.fit['Sb0']))
                 slope = (float(measurement_node.fit['b1']), float(measurement_node.fit['Sb1']))
                 mse = float(measurement_node.mse['mse'])
 
-                yield slope, offset, mse
+                yield input_voltage, slope, offset, mse
 
 
 def get_noise_amp_and_std(soup, configuration, fs):
@@ -42,12 +51,29 @@ def display_dc_mse_results(soup):
 def display_dc_slope_results(soup):
     fig = plt.figure('test')
     for subplot_id, configuration in enumerate(('both', 'both-manual', 'sigdel', 'preamp')):
-        ax = fig.add_subplot(4, 1, subplot_id + 1)
-        for color, fs in (('b', 32), ('r', 96), ('g', 256)):
-            x = 0
-            for slope, offset, mse in get_linearity_results(soup, configuration, fs):
-                ax.scatter(x, slope[0], c=color)
-                x += 1
+        ax = fig.add_subplot(4, 2, subplot_id*2 + 1)
+        ax2 = fig.add_subplot(4, 2, subplot_id*2 + 2)
+        slopes_avg = list()
+        slopes_std = list()
+        sample_frequencies = np.array((32, 96, 256))
+        for fs_id, fs in enumerate(sample_frequencies):
+            slopes = [slope for input_voltage, slope, offset, mse in get_linearity_results(soup, configuration, fs)]
+            slopes = list(list(x) for x in zip(*slopes))
+            avg, std = avg_std(slopes[0], slopes[1])
+            slopes_avg.append(avg)
+            slopes_std.append(std)
+
+            # Offset fs data slightly from one another so they appear in "chunks" in the plot
+            xdata = np.array(range(len(slopes[0])))*1.1-0.05 + fs_id*0.2-0.1
+            ax2.errorbar(xdata, slopes[0], yerr=slopes[1], fmt='o')
+
+        popt, pcov = curve_fit(linear_function, sample_frequencies, slopes_avg, sigma=slopes_std, absolute_sigma=True)
+        perr = np.sqrt(np.diag(pcov))
+        print('avg_std(): {0:.4f} +/- {1:.4f}'.format(*avg_std(slopes_avg, slopes_std)))
+        print('perr: {0:.5f} +/- {1:.8f}'.format(*perr))
+
+        ax.errorbar(sample_frequencies, slopes_avg, yerr=slopes_std, fmt='o')
+        ax.plot(sample_frequencies, linear_function(sample_frequencies, *popt))
         ax.set_title('Slopes for {}'.format(configuration))
     plt.show()
 
@@ -94,6 +120,6 @@ if __name__ == '__main__':
     raw_soup = BeautifulSoup(open('processed_measurements.xml', 'r'), 'xml')
 
     #display_dc_mse_results(fitted_soup)
-    #display_dc_slope_results(fitted_soup)
+    display_dc_slope_results(fitted_soup)
     #display_dc_offset_results(fitted_soup)
-    display_noise_amplitude_and_standard_deviation(fitted_soup)
+    #display_noise_amplitude_and_standard_deviation(fitted_soup)
